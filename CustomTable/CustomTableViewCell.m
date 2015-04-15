72d3cf1a4dc07034dcff007698534a2c1cef61eb
thodep <thodep@yahoo.com>
2015-04-15 14:53:34 -0400

    Finish Chaper 5

--- CustomTable/CustomTableViewCell.m
+++ CustomTable/CustomTableViewCell.m
@@ -10,4 +10,14 @@
 
 @implementation CustomTableViewCell
 
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
+
 @end
--- CustomTable/CustomTableViewController.m
+++ CustomTable/CustomTableViewController.m
@@ -9,10 +9,15 @@
 #import "CustomTableViewController.h"
 #import "CustomTableViewCell.h"
 
+@interface CustomTableViewController ()
+
+@end
 @implementation CustomTableViewController
 {
     NSArray *recipeNames;
     NSArray *recipeImages;
+    NSArray *recipePrepTimes;
+    BOOL recipeChecked[16];
 }
 
 - (void)viewDidLoad {
@@ -50,10 +55,26 @@
                      @"thai_shrimp_cake.jpg",
                      @"angry_birds_cake.jpg",
                      @"ham_and_cheese_panini.jpg"];
-                    }
+    
 
-
-
+recipePrepTimes = [NSArray arrayWithObjects:
+                   @"30 min",
+                   @"30 min",
+                   @"20 min",
+                   @"30 min",
+                   @"10 min",
+                   @"1 hour",
+                   @"45 min",
+                   @"5 min",
+                   @"30 min",
+                   @"8 min",
+                   @"20 min",
+                   @"20 min",
+                   @"5 min",
+                   @"1.5 hour",
+                   @"4 hours",
+                   @"10 min", nil];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -76,8 +97,40 @@
     
     cell.nameLabel.text = [recipeNames objectAtIndex:indexPath.row];
     cell.thumbnailImageView.image = [UIImage imageNamed:[recipeImages objectAtIndex:indexPath.row]];
+    cell.preptimeLabel.text = [recipePrepTimes objectAtIndex:indexPath.row];
+    
+    if (recipeChecked[indexPath.row]) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
     
     return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath
+                                                                    *)indexPath
+
+
+{
+    NSString* selectedRecipe = [recipeNames objectAtIndex:indexPath.row];
+    UIAlertView *messageAlert = [[UIAlertView alloc]
+                                 initWithTitle:@"Row Selected" message:selectedRecipe
+                                 delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+  
+    // Display Alert Message
+    [messageAlert show];
+    
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if (recipeChecked[indexPath.row]) {
+        recipeChecked[indexPath.row] = NO;
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    } else {
+        recipeChecked[indexPath.row] = YES;
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
