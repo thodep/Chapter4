b82485f300685529741705331652d52e42b8adbb
thodep <thodep@yahoo.com>
2015-04-15 10:13:58 -0400

    first commit

--- CustomTable/CustomTableViewController.h
+++ CustomTable/CustomTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CustomTableViewController.h
 //  CustomTable
 //
 //  Created by tho dang on 2015-04-15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CustomTableViewController : UITableViewController
 
 @end
-
---
+++ CustomTable/CustomTableViewController.m
@@ -0,0 +1,82 @@
+//
+//  CustomTableViewController.m
+//  CustomTable
+//
+//  Created by tho dang on 2015-04-15.
+//  Copyright (c) 2015 TD. All rights reserved.
+//
+
+#import "CustomTableViewController.h"
+
+@implementation CustomTableViewController
+{
+    NSArray *recipeNames;
+    NSArray *recipeImages;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    recipeNames = @[@"Egg Benedict",
+                    @"Mushroom Risotto",
+                    @"Full Breakfast",
+                    @"Hamburger",
+                    @"Ham and Egg Sandwich",
+                    @"Creme Brelee",
+                    @"White Chocolate Donut",
+                    @"Starbucks Coffee",
+                    @"Vegetable Curry",
+                    @"Instant Noodle with Egg",
+                    @"Noodle with BBQ Pork",
+                    @"Japanese Noodle with Pork",
+                    @"Green Tea",
+                    @"Thai Shrimp Cake",
+                    @"Angry Birds Cake",
+                    @"Ham and Cheese Panini"];
+    
+    recipeImages = @[@"egg_benedict.jpg"
+                     @"mushroom_risotto.jpg",
+                     @"full_breakfast.jpg",
+                     @"hamburger.jpg",
+                     @"ham_and_egg_sandwich.jpg",
+                     @"creme_brelee.jpg",
+                     @"white_chocolate_donut.jpg",
+                     @"starbucks_coffee.jpg",
+                     @"vegetable_curry.jpg",
+                     @"instant_noodle_with_egg.jpg",
+                     @"noodle_with_bbq_pork.jpg",
+                     @"japanese_noodle_with_pork.jpg",
+                     @"green_tea.jpg",
+                     @"thai_shrimp_cake.jpg",
+                     @"angry_birds_cake.jpg",
+                     @"ham_and_cheese_panini.jpg"];
+                    }
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recipeNames count];
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:
+(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    cell.textLabel.text = [recipeNames objectAtIndex:indexPath.row];
+    
+    cell.imageView.image = [UIImage imageNamed:[recipeImages objectAtIndex:indexPath.row]];
+    
+    return cell;
+}
+
+@end
