45c03f0b0c80256720e57a8fbe896ca0f5cec669
thodep <thodep@yahoo.com>
2015-04-15 09:36:42 -0400

    Initial Commit

---
+++ CustomTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomTable
+//
+//  Created by tho dang on 2015-04-15.
+//  Copyright (c) 2015 TD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomTable
+//
+//  Created by tho dang on 2015-04-15.
+//  Copyright (c) 2015 TD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
