11e3cee1191df19d4ab4aecac7aec45ae715872d
thodep <thodep@yahoo.com>
2015-04-16 17:06:26 -0400

    To be continued at home. Look for different size of Image

--- CustomTable/CustomTableViewController.m
+++ CustomTable/CustomTableViewController.m
@@ -22,58 +22,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    recipeNames = @[@"Egg Benedict",
-                    @"Mushroom Risotto",
-                    @"Full Breakfast",
-                    @"Hamburger",
-                    @"Ham and Egg Sandwich",
-                    @"Creme Brelee",
-                    @"White Chocolate Donut",
-                    @"Starbucks Coffee",
-                    @"Vegetable Curry",
-                    @"Instant Noodle with Egg",
-                    @"Noodle with BBQ Pork",
-                    @"Japanese Noodle with Pork",
-                    @"Green Tea",
-                    @"Thai Shrimp Cake",
-                    @"Angry Birds Cake",
-                    @"Ham and Cheese Panini"];
     
-    recipeImages = @[@"egg_benedict.jpg",
-                     @"mushroom_risotto.jpg",
-                     @"full_breakfast.jpg",
-                     @"hamburger.jpg",
-                     @"ham_and_egg_sandwich.jpg",
-                     @"creme_brelee.jpg",
-                     @"white_chocolate_donut.jpg",
-                     @"starbucks_coffee.jpg",
-                     @"vegetable_curry.jpg",
-                     @"instant_noodle_with_egg.jpg",
-                     @"noodle_with_bbq_pork.jpg",
-                     @"japanese_noodle_with_pork.jpg",
-                     @"green_tea.jpg",
-                     @"thai_shrimp_cake.jpg",
-                     @"angry_birds_cake.jpg",
-                     @"ham_and_cheese_panini.jpg"];
-    
-
-recipePrepTimes = [NSArray arrayWithObjects:
-                   @"30 min",
-                   @"30 min",
-                   @"20 min",
-                   @"30 min",
-                   @"10 min",
-                   @"1 hour",
-                   @"45 min",
-                   @"5 min",
-                   @"30 min",
-                   @"8 min",
-                   @"20 min",
-                   @"20 min",
-                   @"5 min",
-                   @"1.5 hour",
-                   @"4 hours",
-                   @"10 min", nil];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"recipes" ofType:@"plist"];
+    NSDictionary *dict = [[NSDictionary alloc] initWithContentsOfFile:path];
+    recipeNames = [dict objectForKey:@"Name"];
+    recipeImages = [dict objectForKey:@"Image"];
+    recipePrepTimes = [dict objectForKey:@"PrepTime"];
+       
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
