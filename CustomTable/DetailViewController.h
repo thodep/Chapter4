123146343a6d8bac16af9f8701d70b85ae3d764c
thodep <thodep@yahoo.com>
2015-04-16 23:38:23 -0400

    Finish Chapter 9

---
+++ CustomTable/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  CustomTable
+//
+//  Created by tho dang on 2015-04-16.
+//  Copyright (c) 2015 TD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
